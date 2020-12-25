7049604aae0b5455d2973a0f5542e95902b1fdbf
Jon Craton <jncraton@gmail.com>
2020-12-25 18:36:37 -0500

    Change to single-window interactive pattern

--- main.py
+++ main.py
@@ -2,36 +2,39 @@ import os, sys, io
 import PySimpleGUI as sg
 from PIL import Image, ImageEnhance
 
+sg.theme('SystemDefault1')
+
 values = {
     'brightness': 1.0,
     'contrast': 1.0,
 }
 
-# https://pysimplegui.readthedocs.io/en/latest/call%20reference/
-# https://pillow.readthedocs.io/en/5.1.x/reference/index.html
- 
-while True:
-    with Image.open('01.jpg') as im:
+window = sg.Window('Title',
+  [[
+    sg.Image(key='image'),
+    sg.Column([
+        [sg.Text("Brightness"), sg.Slider(range=(0.0,2.0), resolution=.01, default_value=values['brightness'], key="brightness", orientation='h', enable_events=True)],
+        [sg.Text("Contrast"), sg.Slider(range=(0.0,2.0), resolution=.01, default_value=values['contrast'], key="contrast", orientation='h', enable_events=True)],
+    ]),
+  ]])
+
+event, values = window.read(timeout=0)
+
+with Image.open('01.jpg') as im_orig:
+    im_orig.thumbnail((400,300))
+    while True:
+        im = im_orig
         im = ImageEnhance.Brightness(im).enhance(values['brightness'])
         im = ImageEnhance.Contrast(im).enhance(values['contrast'])
-        im.thumbnail((400,300))
         b = io.BytesIO()
         im.save(b, 'PNG')
         image_bytes = b.getvalue()
 
-        print(values)
-
-        event, values = sg.Window('Title',
-          [[
-            sg.Image(data=image_bytes),
-            sg.Column([
-                [sg.Text("Brightness"), sg.Slider(range=(0.0,2.0), resolution=.01, default_value=values['brightness'], key="brightness", orientation='h', enable_events=True)],
-                [sg.Text("Contrast"), sg.Slider(range=(0.0,2.0), resolution=.01, default_value=values['contrast'], key="contrast", orientation='h')],
-            ]),
-          ],
-          [sg.Submit()]]).read(close=True)
+        window['image'].update(data=image_bytes)
 
         print(event, values)
 
-        if not event:
-            exit(0)
+        event, values = window.read()
+
+        if event == sg.WIN_CLOSED or event == 'Exit':
+                break
