90fc87787c9790cf9837e816b2e0d34119f7db96
Jon Craton <jncraton@gmail.com>
2020-12-25 18:38:32 -0500

    Reformat with black

--- main.py
+++ main.py
@@ -2,37 +2,63 @@ import os, sys, io
 import PySimpleGUI as sg
 from PIL import Image, ImageEnhance
 
-sg.theme('SystemDefault1')
+sg.theme("SystemDefault1")
 
 values = {
-    'brightness': 1.0,
-    'contrast': 1.0,
+    "brightness": 1.0,
+    "contrast": 1.0,
 }
 
-window = sg.Window('Title',
-  [[
-    sg.Image(key='image'),
-    sg.Column([
-        [sg.Text("Brightness"), sg.Slider(range=(0.0,2.0), resolution=.01, default_value=values['brightness'], key="brightness", orientation='h', enable_events=True)],
-        [sg.Text("Contrast"), sg.Slider(range=(0.0,2.0), resolution=.01, default_value=values['contrast'], key="contrast", orientation='h', enable_events=True)],
-    ]),
-  ]])
+window = sg.Window(
+    "Title",
+    [
+        [
+            sg.Image(key="image"),
+            sg.Column(
+                [
+                    [
+                        sg.Text("Brightness"),
+                        sg.Slider(
+                            range=(0.0, 2.0),
+                            resolution=0.01,
+                            default_value=values["brightness"],
+                            key="brightness",
+                            orientation="h",
+                            enable_events=True,
+                        ),
+                    ],
+                    [
+                        sg.Text("Contrast"),
+                        sg.Slider(
+                            range=(0.0, 2.0),
+                            resolution=0.01,
+                            default_value=values["contrast"],
+                            key="contrast",
+                            orientation="h",
+                            enable_events=True,
+                        ),
+                    ],
+                ]
+            ),
+        ]
+    ],
+)
 
 event, values = window.read(timeout=0)
 
-with Image.open('01.jpg') as im_orig:
-    im_orig.thumbnail((400,300))
+with Image.open("01.jpg") as im_orig:
+    im_orig.thumbnail((400, 300))
     while True:
         im = im_orig
-        im = ImageEnhance.Brightness(im).enhance(values['brightness'])
-        im = ImageEnhance.Contrast(im).enhance(values['contrast'])
+        im = ImageEnhance.Brightness(im).enhance(values["brightness"])
+        im = ImageEnhance.Contrast(im).enhance(values["contrast"])
         b = io.BytesIO()
-        im.save(b, 'PNG')
+        im.save(b, "PNG")
         image_bytes = b.getvalue()
 
-        window['image'].update(data=image_bytes)
+        window["image"].update(data=image_bytes)
 
         event, values = window.read()
 
-        if event == sg.WIN_CLOSED or event == 'Exit':
-                break
+        if event == sg.WIN_CLOSED or event == "Exit":
+            break
