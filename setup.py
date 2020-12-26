034bb149d5d067014b84229bcaa1281fcd8d58fe
Jon Craton <jncraton@gmail.com>
2020-12-25 20:47:46 -0500

    Rev version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("readme.md", "r") as fh:
 
 setuptools.setup(
     name="imbaedit",
-    version="1.0.0",
+    version="1.0.1",
     author="Jon Craton",
     author_email="jon@joncraton.com",
     description="Image Batch Editor",
