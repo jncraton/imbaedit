3f885dcdd3a919e43f30d13674ec28c7ef730e92
Jon Craton <jncraton@gmail.com>
2020-12-26 15:38:12 -0500

    Remove debug output

--- imbaedit/imbaedit.py
+++ imbaedit/imbaedit.py
@@ -2,29 +2,27 @@ import os, sys, io
 from tkinter import *
 from PIL import Image, ImageEnhance
 
-
 def set_white_level(pixel, level):
     if sum(pixel) > level * 255 * 3:
         return (255, 255, 255)
     else:
         return pixel
 
-
 def autocrop(image):
     box = [0, 0, 0, 0]
 
     for x in range(image.width):
         for y in range(image.height):
-            if image.getpixel((x, y)) != (255, 255, 255):
+            if image.getpixel((x,y)) != (255,255,255):
                 box[0] = x
                 break
         else:
             continue
         break
 
-    for x in range(image.width - 1, -1, -1):
+    for x in range(image.width-1, -1, -1):
         for y in range(image.height):
-            if image.getpixel((x, y)) != (255, 255, 255):
+            if image.getpixel((x,y)) != (255,255,255):
                 box[2] = x
                 break
         else:
@@ -33,25 +31,23 @@ def autocrop(image):
 
     for y in range(image.height):
         for x in range(image.width):
-            if image.getpixel((x, y)) != (255, 255, 255):
+            if image.getpixel((x,y)) != (255,255,255):
                 box[1] = y
                 break
         else:
             continue
         break
 
-    for y in range(image.height - 1, -1, -1):
+    for y in range(image.height-1, -1, -1):
         for x in range(image.width):
-            if image.getpixel((x, y)) != (255, 255, 255):
+            if image.getpixel((x,y)) != (255,255,255):
                 box[3] = y
                 break
         else:
             continue
         break
 
-    print(box)
     return image.crop(tuple(box))
-
 
 def apply_filters(im, values):
     im = ImageEnhance.Brightness(im).enhance(values["brightness"].get())
@@ -71,9 +67,8 @@ def run():
         im = apply_filters(Image.open(sys.argv[1]), values)
         im.save(file)
 
-
 def main():
-    window = Tk()
+    window = Tk()    
 
     values = {
         "brightness": DoubleVar(window, 1.0),
@@ -93,75 +88,29 @@ def main():
 
         photoimage = PhotoImage(data=b.getvalue())
         img.configure(image=photoimage)
-        img.image = photoimage
+        img.image=photoimage
 
     window.title("imbaedit")
 
     img = Label(window, width=400, height=300)
     img.grid(column=0, row=0)
 
-    Scale(
-        window,
-        from_=0.0,
-        to=2.0,
-        resolution=0.01,
-        orient=HORIZONTAL,
-        variable=values["brightness"],
-        label="Brightness",
-        command=update,
-    ).grid(column=0, row=1)
-
-    Scale(
-        window,
-        from_=0.0,
-        to=2.0,
-        resolution=0.01,
-        orient=HORIZONTAL,
-        variable=values["contrast"],
-        label="Contrast",
-        command=update,
-    ).grid(column=0, row=2)
-
-    Scale(
-        window,
-        from_=0.0,
-        to=2.0,
-        resolution=0.01,
-        orient=HORIZONTAL,
-        variable=values["saturation"],
-        label="Saturation",
-        command=update,
-    ).grid(column=0, row=3)
-
-    Scale(
-        window,
-        from_=0.0,
-        to=2.0,
-        resolution=0.01,
-        orient=HORIZONTAL,
-        variable=values["sharpness"],
-        label="Sharpness",
-        command=update,
-    ).grid(column=0, row=4)
-
-    Scale(
-        window,
-        from_=0.0,
-        to=2.0,
-        resolution=0.01,
-        orient=HORIZONTAL,
-        variable=values["white"],
-        label="White",
-        command=update,
-    ).grid(column=0, row=5)
+    Scale(window, from_=0.0, to=2.0, resolution=.01, orient=HORIZONTAL, variable=values['brightness'], label="Brightness", command=update).grid(column=0, row=1)
+    
+    Scale(window, from_=0.0, to=2.0, resolution=.01, orient=HORIZONTAL, variable=values['contrast'], label="Contrast", command=update).grid(column=0, row=2)
+    
+    Scale(window, from_=0.0, to=2.0, resolution=.01, orient=HORIZONTAL, variable=values['saturation'], label="Saturation", command=update).grid(column=0, row=3)
+    
+    Scale(window, from_=0.0, to=2.0, resolution=.01, orient=HORIZONTAL, variable=values['sharpness'], label="Sharpness", command=update).grid(column=0, row=4)
+    
+    Scale(window, from_=0.0, to=2.0, resolution=.01, orient=HORIZONTAL, variable=values['white'], label="White", command=update).grid(column=0, row=5)
 
     btn = Button(window, text="Run", font=("Arial Bold", 16), command=run)
     btn.grid(column=0, row=6)
 
     update()
-
+    
     window.mainloop()
-
 
 if __name__ == "__main__":
     main()
